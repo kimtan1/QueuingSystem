348f6b1aa72e631fc4636398321612b17248005a
kimtan1 <lekimtan.250600@gmail.com>
2022-05-12 20:11:52 +0700

    addfile

---
+++ src/Components/Layout/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import "./dashboard.scss"
+
+const Dashboard : React.FC = () => {
+  return (
+      <div className="app__layout-dashboard">
+          DASHBOARD
+      </div>
+  )
+}
+
+export default Dashboard
---
+++ src/Pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Textbox from '../../Components/Textbox/Textbox'
+import UserSection from '../../Components/UserSection/UserSection'
+import "./dashboardpage.scss"
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div className="app__dashboard-page">
+      <Sidebar/>
+      <UserSection/>
+      <div className="dashboard__content">
+        {/* ban beo cot' vao` day */}
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardPage
